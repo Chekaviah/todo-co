a8aac3e65faa7c6f6fc6918738c2674619938649
Chekaviah <chekaviah@gmail.com>
2018-02-23 17:52:34 +0100

    add entities relations and respect psr-1

--- src/AppBundle/Entity/Task.php
+++ src/AppBundle/Entity/Task.php
@@ -2,16 +2,23 @@
 
 namespace AppBundle\Entity;
 
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
+ * Class User
+ *
+ * @author Mathieu GUILLEMINOT <guilleminotm@gmail.com>
+ *
  * @ORM\Entity
  * @ORM\Table
  */
 class Task
 {
     /**
+     * @var int
+     *
      * @ORM\Column(type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
@@ -19,75 +26,137 @@ class Task
     private $id;
 
     /**
+     * @var DateTime
+     *
      * @ORM\Column(type="datetime")
      */
-    private $createdAt;
+    private $created;
 
     /**
+     * @var string
+     *
      * @ORM\Column(type="string")
      * @Assert\NotBlank(message="Vous devez saisir un titre.")
      */
     private $title;
 
     /**
+     * @var string
+     *
      * @ORM\Column(type="text")
      * @Assert\NotBlank(message="Vous devez saisir du contenu.")
      */
     private $content;
 
     /**
+     * @var bool
+     *
      * @ORM\Column(type="boolean")
      */
-    private $isDone;
+    private $status;
 
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User", inversedBy="tasks")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $user;
+
+    /**
+     * Task constructor.
+     */
     public function __construct()
     {
-        $this->createdAt = new \Datetime();
-        $this->isDone = false;
+        $this->createdAt = new Datetime();
+        $this->status = false;
     }
 
+    /**
+     * @return int
+     */
     public function getId()
     {
         return $this->id;
     }
 
-    public function getCreatedAt()
+    /**
+     * @return DateTime
+     */
+    public function getCreated()
     {
-        return $this->createdAt;
+        return $this->created;
     }
 
-    public function setCreatedAt($createdAt)
+    /**
+     * @param DateTime $created
+     */
+    public function setCreated($created)
     {
-        $this->createdAt = $createdAt;
+        $this->createdAt = $created;
     }
 
+    /**
+     * @return string
+     */
     public function getTitle()
     {
         return $this->title;
     }
 
+    /**
+     * @param string $title
+     */
     public function setTitle($title)
     {
         $this->title = $title;
     }
 
+    /**
+     * @return string
+     */
     public function getContent()
     {
         return $this->content;
     }
 
+    /**
+     * @param string $content
+     */
     public function setContent($content)
     {
         $this->content = $content;
     }
 
-    public function isDone()
+    /**
+     * @return bool
+     */
+    public function getStatus()
     {
-        return $this->isDone;
+        return $this->status;
     }
 
-    public function toggle($flag)
+    /**
+     * @param bool $status
+     */
+    public function setStatus($status)
     {
-        $this->isDone = $flag;
+        $this->status = $status;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
     }
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,11 +3,16 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
+ * Class User
+ *
+ * @author Mathieu GUILLEMINOT <guilleminotm@gmail.com>
+ *
  * @ORM\Table("user")
  * @ORM\Entity
  * @UniqueEntity("email")
@@ -15,73 +20,188 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 class User implements UserInterface
 {
     /**
-     * @ORM\Column(type="integer")
+     * @var int
+     *
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
+     * @var string
+     *
      * @ORM\Column(type="string", length=25, unique=true)
      * @Assert\NotBlank(message="Vous devez saisir un nom d'utilisateur.")
      */
     private $username;
 
     /**
+     * @var string
+     */
+    private $plainPassword;
+
+    /**
+     * @var string
+     *
      * @ORM\Column(type="string", length=64)
      */
     private $password;
 
     /**
+     * @var string
+     *
      * @ORM\Column(type="string", length=60, unique=true)
      * @Assert\NotBlank(message="Vous devez saisir une adresse email.")
      * @Assert\Email(message="Le format de l'adresse n'est pas correcte.")
      */
     private $email;
 
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="roles", type="json_array")
+     */
+    private $roles = [];
+
+    /**
+     * @var Task[]|ArrayCollection
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Task", cascade={"persist", "refresh"}, mappedBy="user")
+     */
+    private $tasks;
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        $this->tasks = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * @return string
+     */
     public function getUsername()
     {
         return $this->username;
     }
 
+    /**
+     * @param string $username
+     */
     public function setUsername($username)
     {
         $this->username = $username;
     }
 
-    public function getSalt()
+    /**
+     * @return string
+     */
+    public function getPlainPassword()
     {
-        return null;
+        return $this->plainPassword;
     }
 
+    /**
+     * @param string $plainPassword
+     */
+    public function setPlainPassword($plainPassword)
+    {
+        $this->plainPassword = $plainPassword;
+    }
+
+    /**
+     * @return string
+     */
     public function getPassword()
     {
         return $this->password;
     }
 
+    /**
+     * @param string $password
+     */
     public function setPassword($password)
     {
         $this->password = $password;
     }
 
+    /**
+     * @return string
+     */
     public function getEmail()
     {
         return $this->email;
     }
 
+    /**
+     * @param string $email
+     */
     public function setEmail($email)
     {
         $this->email = $email;
     }
 
+    /**
+     * @return array
+     */
     public function getRoles()
     {
-        return array('ROLE_USER');
+        $roles = $this->roles;
+
+        if (empty($roles))
+            $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param array $roles
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+    }
+
+    /**
+     * @param Task $task
+     */
+    public function addTask($task)
+    {
+        $this->tasks[] = $task;
+        $task->setUser($this);
+    }
+
+    /**
+     * @param Task $task
+     */
+    public function removeTask($task)
+    {
+        $this->tasks->removeElement($task);
+    }
+
+    /**
+     * @return ArrayCollection|Task[]
+     */
+    public function getTasks()
+    {
+        return $this->tasks;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSalt()
+    {
+        return null;
     }
 
     public function eraseCredentials()
