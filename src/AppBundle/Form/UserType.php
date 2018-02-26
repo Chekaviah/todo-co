fab73e0d2e0fc8e64bb67b380fa217916d64c1f7
Chekaviah <chekaviah@gmail.com>
2018-02-26 10:52:02 +0100

    add roles choice in user create/edit forms

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -3,11 +3,14 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class UserType extends AbstractType
 {
@@ -23,6 +26,28 @@ class UserType extends AbstractType
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
             ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add('roles', ChoiceType::class, array(
+                'choices' => [
+                    'ROLE_ADMIN' => 'ROLE_ADMIN',
+                    'ROLE_USER' => 'ROLE_USER',
+                ],
+                'expanded' => true,
+                'multiple' => true,
+            ))
+            ->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+                $user = $event->getData();
+                $form = $event->getForm();
+
+                if ($user->getId() !== null) {
+                    $form->add('password', RepeatedType::class, [
+                        'type'            => PasswordType::class,
+                        'invalid_message' => 'Les deux mots de passe doivent correspondre.',
+                        'required'        => false,
+                        'first_options'   => ['label' => 'Mot de passe'],
+                        'second_options'  => ['label' => 'Tapez le mot de passe à nouveau'],
+                    ]);
+                }
+            });
         ;
     }
 }
